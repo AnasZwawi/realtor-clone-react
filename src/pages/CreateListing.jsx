da0f07d1858725d7739927e049a5d5e510b174f8
AnasZwawi <107753978+AnasZwawi@users.noreply.github.com>
2024-01-03 15:48:52 +0100

    added some functionality to listing page

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -3,21 +3,54 @@ import React, { useState } from "react";
 function CreateListing() {
   const [formData, setFormData] = useState({
     type: "rent",
-    title: "",
+    name: "",
     bedrooms: 0,
     bathrooms: 1,
     parking: false,
     furnished: false,
-    address: "",
-    description : "",
+    address: '',
+    description: '',
     offer: false,
     regularPrice: 0,
     discountedPrice: 0,
   });
 
-  const { type, title, bedrooms, bathrooms, parking, furnished, address, description, offer, regularPrice, discountedPrice } = formData;
+  const {
+    type,
+    name,
+    bedrooms,
+    bathrooms,
+    parking,
+    furnished,
+    address,
+    description,
+    offer,
+    regularPrice,
+    discountedPrice,
+  } = formData;
 
-  function onChangeHandler() {}
+  function onChangeHandler(event) {
+    console.log(event.target.value);
+    let boolean = null;
+    if (event.target.value === "true") {
+      boolean = true;
+    }
+    if (event.target.value === "false") {
+      boolean = false;
+    }
+    if (event.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        images: event.target.files,
+      }));
+    }
+    if (!event.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        [event.target.id]: boolean ?? event.target.value,
+      }));
+    }
+  }
 
   return (
     <main className="max-w-md px-2 mx-auto">
@@ -28,6 +61,7 @@ function CreateListing() {
         <p className="text-lg mt-6 font-semibold text-gray-800">Sell / Rent</p>
         <div className="flex space-x-5">
           <button
+            onClick={onChangeHandler}
             type="button"
             id="type"
             value="sale"
@@ -40,11 +74,12 @@ function CreateListing() {
             sell
           </button>
           <button
+            onClick={onChangeHandler}
             type="button"
             id="type"
-            value="sale"
+            value="rent"
             className={`px-7 py-3 font-semibold text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${
-              type === "sell"
+              type === "sale"
                 ? "bg-white text-gray-700"
                 : "bg-slate-600 text-white"
             }`}
@@ -54,10 +89,11 @@ function CreateListing() {
         </div>
         <p className="text-lg mt-6 font-semibold text-gray-800">Title</p>
         <input
+          type="text"
           className="w-full rounded transition duration-150 ease-in-out text-gray-700 font-normal text-lg border-gray-300"
-          id="title"
+          id="name"
           placeholder="Title"
-          value={title}
+          value={name}
           onChange={onChangeHandler}
           maxLength={"32"}
           minLength={"6"}
@@ -78,7 +114,9 @@ function CreateListing() {
             />
           </div>
           <div className="w-full">
-            <p className="text-lg mt-6 font-semibold text-gray-800">Bathrooms</p>
+            <p className="text-lg mt-6 font-semibold text-gray-800">
+              Bathrooms
+            </p>
             <input
               className="w-full rounded transition duration-150 ease-in-out text-gray-700 font-normal text-lg border-gray-300"
               id="bathrooms"
@@ -94,25 +132,23 @@ function CreateListing() {
         <p className="text-lg mt-6 font-semibold text-gray-800">Parking Spot</p>
         <div className="flex space-x-5">
           <button
+            onClick={onChangeHandler}
             type="button"
             id="parking"
-            value={parking}
+            value={true}
             className={`px-7 py-3 font-semibold text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${
-              !parking
-                ? "bg-white text-gray-700"
-                : "bg-slate-600 text-white"
+              !parking ? "bg-white text-gray-700" : "bg-slate-600 text-white"
             }`}
           >
             Yes
           </button>
           <button
+            onClick={onChangeHandler}
             type="button"
             id="parking"
-            value={parking}
+            value={false}
             className={`px-7 py-3 font-semibold text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${
-              parking
-                ? "bg-white text-gray-700"
-                : "bg-slate-600 text-white"
+              parking ? "bg-white text-gray-700" : "bg-slate-600 text-white"
             }`}
           >
             No
@@ -121,25 +157,23 @@ function CreateListing() {
         <p className="text-lg mt-6 font-semibold text-gray-800">Furnished</p>
         <div className="flex space-x-5">
           <button
+            onClick={onChangeHandler}
             type="button"
             id="furnished"
-            value={furnished}
+            value={true}
             className={`px-7 py-3 font-semibold text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${
-              !furnished
-                ? "bg-white text-gray-700"
-                : "bg-slate-600 text-white"
+              !furnished ? "bg-white text-gray-700" : "bg-slate-600 text-white"
             }`}
           >
             yes
           </button>
           <button
+            onClick={onChangeHandler}
             type="button"
             id="furnished"
-            value={furnished}
+            value={false}
             className={`px-7 py-3 font-semibold text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${
-              furnished
-                ? "bg-white text-gray-700"
-                : "bg-slate-600 text-white"
+              furnished ? "bg-white text-gray-700" : "bg-slate-600 text-white"
             }`}
           >
             No
@@ -147,57 +181,58 @@ function CreateListing() {
         </div>
         <p className="text-lg mt-6 font-semibold text-gray-800">Address</p>
         <textarea
+          type="text"
           className="w-full rounded transition duration-150 ease-in-out text-gray-700 font-normal text-lg border-gray-300"
-          id="Address"
+          id="address"
           placeholder="Address"
           value={address}
           onChange={onChangeHandler}
-          maxLength={"32"}
-          minLength={"6"}
           required
         />
         <p className="text-lg mt-6 font-semibold text-gray-800">Description</p>
         <textarea
+          type="text"
           className="w-full rounded transition duration-150 ease-in-out text-gray-700 font-normal text-lg border-gray-300"
-          id="Description"
+          id="description"
           placeholder="Description"
           value={description}
           onChange={onChangeHandler}
-          maxLength={"32"}
-          minLength={"6"}
           required
         />
         <p className="text-lg mt-6 font-semibold text-gray-800">Offer</p>
         <div className="flex space-x-5">
           <button
+            onClick={onChangeHandler}
             type="button"
             id="offer"
-            value={offer}
+            value={true}
             className={`px-7 py-3 font-semibold text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${
-              !offer
-                ? "bg-white text-gray-700"
-                : "bg-slate-600 text-white"
+              !offer ? "bg-white text-gray-700" : "bg-slate-600 text-white"
             }`}
           >
             yes
           </button>
           <button
+            onClick={onChangeHandler}
             type="button"
             id="offer"
-            value={offer}
+            value={false}
             className={`px-7 py-3 font-semibold text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${
-              offer
-                ? "bg-white text-gray-700"
-                : "bg-slate-600 text-white"
+              offer ? "bg-white text-gray-700" : "bg-slate-600 text-white"
             }`}
           >
             No
           </button>
-
         </div>
         <div className="w-full flex space-x-5">
           <div className="w-full">
-            <p className="text-lg mt-6 font-semibold text-gray-800">Regular Price<span className="text-sm mt-6 font-normal text-gray-800"> /Month</span> </p>
+            <p className="text-lg mt-6 font-semibold text-gray-800">
+              Regular Price
+              <span className="text-sm mt-6 font-normal text-gray-800">
+                {" "}
+                /Month
+              </span>{" "}
+            </p>
             <input
               className="w-full rounded transition duration-150 ease-in-out text-gray-700 font-normal text-lg border-gray-300"
               id="regularPrice"
@@ -209,7 +244,9 @@ function CreateListing() {
             />
           </div>
           <div className="w-full">
-            <p className="text-lg mt-6 font-semibold text-gray-800">Discounted Price </p>
+            <p className="text-lg mt-6 font-semibold text-gray-800">
+              Discounted Price{" "}
+            </p>
             <input
               className="w-full rounded transition duration-150 ease-in-out text-gray-700 font-normal text-lg border-gray-300"
               id="discountedPrice"
@@ -223,18 +260,25 @@ function CreateListing() {
         </div>
         <div>
           <p className="text-lg mt-6 font-semibold text-gray-800">Images </p>
-          <p className="text-sm font-normal text-gray-600">The first image will be the cover (max 6). </p>
+          <p className="text-sm font-normal text-gray-600">
+            The first image will be the cover (max 6).{" "}
+          </p>
           <input
-              className="w-full rounded transition duration-150 ease-in-out bg-white p-2 text-gray-700 font-normal text-lg border border-gray-300 focus:bg-white focus:border-slate-600"
-              id="images"
-              type="file"
-              onChange={onChangeHandler}
-              required
-              multiple
-              accept=".jpg, .jpeg, .png, .webp"
-            />
+            className="w-full rounded transition duration-150 ease-in-out bg-white p-2 text-gray-700 font-normal text-lg border border-gray-300 focus:bg-white focus:border-slate-600"
+            id="images"
+            type="file"
+            onChange={onChangeHandler}
+            required
+            multiple
+            accept=".jpg, .jpeg, .png, .webp"
+          />
         </div>
-        <button type="submit" className="w-full mt-6 mb-10 text-white bg-blue-600 p-[12px] text-[15px] font-medium rounded hover:bg-blue-700 transition duration-150 ease-in-out active:bg-blue-900 shadow-md hover:shadow-lg" >CREATE LISTING</button>
+        <button
+          type="submit"
+          className="w-full mt-6 mb-10 text-white bg-blue-600 p-[12px] text-[15px] font-medium rounded hover:bg-blue-700 transition duration-150 ease-in-out active:bg-blue-900 shadow-md hover:shadow-lg"
+        >
+          CREATE LISTING
+        </button>
       </form>
     </main>
   );
